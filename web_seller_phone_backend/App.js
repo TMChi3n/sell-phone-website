553cb0185cbc7aa90852c46c7983b914e255c9d7
TMChi3n <tranminhchien15052003@gmail.com>
2024-04-21 22:21:36 +0700

    Remove Cart

--- web_seller_phone_backend/App.js
+++ web_seller_phone_backend/App.js
@@ -2,9 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import ProductRoutes from './routes/ProductRoutes.js';
 import UserRoute from './routes/UserRoute.js';
-import CartRoutes from './routes/CartRoutes.js';
 import cors from 'cors'; // Import gói cors
-
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -15,9 +13,6 @@ app.use(express.json());
 
 app.use('/api', ProductRoutes);
 app.use('/api', UserRoute);
-app.use('/api', CartRoutes);
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on http://${port}`);
