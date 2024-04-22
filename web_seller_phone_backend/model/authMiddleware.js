15251c98001679ce412c3afbcf8c93f582966ac4
xuanvietcode <txviet2003@gmail.com>
2024-04-22 09:30:34 +0700

    hoàn thành addtocart và getCartItems

--- web_seller_phone_backend/model/authMiddleware.js
+++ web_seller_phone_backend/model/authMiddleware.js
@@ -1,27 +1,21 @@
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config(); // Đảm bảo rằng biến môi trường được nạp
-
+dotenv.config();
 const authMiddleware = (req, res, next) => {
-  // Lấy token từ tiêu đề Authorization với định dạng "Bearer <token>"
   const authorizationHeader = req.headers.authorization;
-
   if (!authorizationHeader) {
-    return res.status(401).json({ message: 'Authorization header is missing' }); // Xử lý khi không có tiêu đề
+    return res.status(401).json({ message: 'Authorization header is missing' });
   }
-
-  const token = authorizationHeader.split(' ')[1]; // Lấy token từ phần thứ hai sau "Bearer"
-
+  const token = authorizationHeader.split(' ')[1];
   if (!token) {
-    return res.status(401).json({ message: 'Token is missing' }); // Xử lý khi không có token
+    return res.status(401).json({ message: 'Token is missing' });
   }
-
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN);
     req.id_user = decoded.payload.id_user;
     next(); 
   } catch (error) { 
-    return res.status(401).json({ message: error.message || 'Invalid token' }); // Xử lý lỗi
+    return res.status(401).json({ message: error.message || 'Invalid token' });
   }
 }
 
