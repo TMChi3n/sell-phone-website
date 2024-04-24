dff2fb5562188cd2e55f7983e2cb8a7b794032ff
TMChi3n <tranminhchien15052003@gmail.com>
2024-04-24 23:07:32 +0700

    fix delete product by id

--- web_seller_phone/src/page/SignInPage/SignInPage.jsx
+++ web_seller_phone/src/page/SignInPage/SignInPage.jsx
@@ -99,7 +99,7 @@ function SignInPage() {
                     <h1>Xin chào</h1>
                     <p style={{ marginBottom: '50px' }}>Đăng nhập vào tài khoản bằng email</p>
                     <InputForm onChange={setEmail} value={email} style={{ marginBottom: '10px' }} placeholder="Email" />
-                    <InputForm onChange={setPassword} value={password} placeholder="password" />
+                    <InputForm onChange={setPassword} type="password" value={password} placeholder="password" />
                     <Button
                         onClick={handleSignIn}
                         disable={!email || !password ? true : false}
